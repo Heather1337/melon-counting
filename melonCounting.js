0fdd41850a9d9cdb6380b17499fa01437baa73eb
Heather1337 <heatherlynn1337@gmail.com>
2020-10-20 20:01:22 -0700

    Refactoring the Python file to Javascript.

---
+++ melonCounting.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+const countMelons = (melonList) => {
+    /* Takes in a list of melons and returns an object with the count by melon type. */
+
+    const melonCount = {};
+
+    for (const melon of melonList) {
+        if(!(melon in melonCount)) {
+            melonCount[melon] = 1;
+        } else {
+            melonCount[melon]++;
+        }
+    }
+
+    return melonCount;
+};
+
+console.log(countMelons(melons_to_add))
+
+
